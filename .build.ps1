441b09e22fbc6a1724fd378c85badbf67d52402d
Scott Matthews <scottmatthews343@gmail.com>
2020-06-01 03:57:45 -0700

    tidy up and publish 0.1.1 (#2)
    * tidy up and publish 0.1.1
    
    * wip
    
    * wip

--- .build.ps1
+++ .build.ps1
@@ -1,24 +1,33 @@
+Assert ($PSVersionTable.PSVersion.Major -ge 7) "Powershell 7.0 or greater required!"
+
 $moduleName = 'PSRabbitMq.Consumer'
+$pidFile = Join-Path $BuildRoot '.pid'
 $moduleDir = Join-Path $BuildRoot "src/$moduleName"
 $moduleTests = Join-Path $BuildRoot "src/$moduleName.Tests"
-$publishDir = Join-Path $moduleDir "/bin/Debug/netstandard2.1/publish"
-$moduleDll = Join-Path $publishDir "$moduleName.dll"
-$modulePsd1 = Join-Path $publishDir "$moduleName.psd1"
-$pidFile = Join-Path $BuildRoot '.pid'
-$testOutputDir = Join-Path $BuildRoot ".nunit"
+$modulePsd1 = Join-Path $moduleDir "$moduleName.psd1"
+$moduleVersion = [System.Version]::New((Get-Content $modulePsd1 -Raw | Invoke-Expression).ModuleVersion)
+$publishRoot = Join-Path $BuildRoot "publish"
+$publishDir = Join-Path $publishRoot "$moduleName/$moduleVersion"
+$publishDll = Join-Path $publishDir "$moduleName.dll"
+$publishPsd1 = Join-Path $publishDir "$moduleName.psd1"
+
+$buildModuleParams = @{
+    Inputs = {(Get-ChildItem "$moduleDir/*.cs" -Recurse) + (Get-ChildItem "$moduleDir/$moduleName.csproj")}
+    Outputs = $publishDll
+}
 
 task . ClosePSCore,BuildModule,RunPSCore
 
-$buildModuleParams = @{
-    Inputs = {(Get-ChildItem "$moduleDir/*.cs" -Recurse) + (Get-ChildItem "$moduleDir/$moduleName.csproj", "$moduleDir/$moduleName.psd1")}
-    Outputs = $moduleDll
-}
-
 task BuildModule @buildModuleParams {
-    exec { dotnet publish "$moduleDir"}
+    exec { dotnet publish "$moduleDir" --output $publishDir}
+    Copy-Item -Path $modulePsd1 -Destination $publishPsd1
 }
 
-
+task PublishModule BuildModule,{
+    Assert ($env:NUGET_API_KEY) "Missing 'NUGET_API_KEY' environment variabe!"
+    [System.Environment]::SetEnvironmentVariable("PSModulePath", $publishRoot)
+    Publish-Module -Name $moduleName -NuGetApiKey $env:NUGET_API_KEY -RequiredVersion $moduleVersion
+}
 
 task RunPSCore {
     $psCore = @{
@@ -27,7 +36,7 @@ task RunPSCore {
         ArgumentList = @(
             '-NoExit'
             '-Command'
-            '"ipmo {0}; gcm -mod {1}"' -f $modulePsd1, $moduleName
+            '"ipmo {0}; gcm -mod {1}"' -f $publishPsd1, $moduleName
         )
     }
     $psCore
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # PSRabbitMq.Consumer
+This module aims to extend [PSRabbitMQ](https://github.com/RamblingCookieMonster/PSRabbitMq) and focuses on consumption of messages only. For configuring rabbitmq and publishing messages I recommend using PSRabbitMQ or other client library.
+
+This module is built for dotnet core and powershell 7.0+ on windows and linux. 
+
+## Usage
+see [examples](./examples) here.
+
+## Contributing
+To build this module locally you will need:
+- powershell 7 or later
+- dotnet sdk 3.1 or later
+
+```powershell
+# compile module after making changes
+# this will open a new powershell window and load the module for immediate use
+Invoke-Build
+
+# run unit tests locally
+Invoke-Build UnitTests
+```
